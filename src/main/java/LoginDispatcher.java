814fe320ae6c1f3864ca0d19554034f7f86491bd
Sonia Kaul <soniakau@usc.edu>
2022-05-02 17:54:07 -0700

    Update LoginDispatcher.java
    updating loginDispatcher for TICTACTOE, remember to change landingPage to whatever home page is

--- src/main/java/LoginDispatcher.java
+++ src/main/java/LoginDispatcher.java
@@ -1,90 +1,94 @@
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import Util.Constant;
-
 import java.io.IOException;
-import java.io.Serial;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Util.Constant;
+
 /**
  * Servlet implementation class LoginDispatcher
  */
-
-@WebServlet("/Login")
+@WebServlet("/LoginDispatcher")
 public class LoginDispatcher extends HttpServlet {
-    @Serial
-    private static final long serialVersionUID = 1L;
-
+	private static final long serialVersionUID = 1L;
+	private static String landingPage = "index.jsp";  // replace with correct logged in landing page
+       
     /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     * response)
+     * Default Constructor
      */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	String email = request.getParameter("email");
-    	String password = request.getParameter("password");
+    public LoginDispatcher() {
+    	
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String error = ""; 
+    	/**
+    	 * check if email and password exist in database
+    	 * if logged in -> send to ________
+    	 * if not -> send error and send to auth.jsp
+    	 */
+    	String email = request.getParameter("login_email");
+    	String password = request.getParameter("login_password");
     	
     	try {
-    		Class.forName("com.mysql.cj.jdbc.Driver");
-    	} catch (Exception ex) {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (Exception ex) {
     		ex.printStackTrace();
     	}
     	
-        String sql = "SELECT * FROM Users WHERE email = ?";
-        try (
-    			Connection conn = DriverManager.getConnection(Constant.DBAddress, Constant.DBUserName, Constant.DBPassword);
-    			PreparedStatement st = conn.prepareStatement(sql);
+    	String find_sql = "SELECT * FROM Users WHERE email=?"; 
+    	try (
+    			Connection connection = DriverManager.getConnection(Constant.url, Constant.DBUserName, Constant.DBPassword);
+    			PreparedStatement ps = connection.prepareStatement(find_sql);
     		) {
-        	st.setString(1, email);
-        	ResultSet rs = st.executeQuery();
-        
+        	ps.setString(1, email);
+        	ResultSet rs = ps.executeQuery();
+        	
+        	// checking if the email is in the database
         	if (rs.next()) {
-        		String dbpassword = rs.getString("password");
-        		if (dbpassword.equals(password)) {
-        			// Successful login, send cookie
-        			Cookie auth = new Cookie("auth", "true");
-        			Cookie name = new Cookie("name", rs.getString("name").replace(' ', '+'));
-        			response.addCookie(auth);
-        			response.addCookie(name);
-        			response.sendRedirect("index.jsp");
-        		} else {
-        			// Incorrect Password
-        			sendError("Password is incorrect.", request, response);
+        		// checking if the input password matches the database password
+        		String login_password = rs.getString("password"); 
+        		if (login_password.equals(password)) {
+        			// user is logged in 
+        			// add cookies for name display
+//        			Cookie username = new Cookie("name", rs.getString("name").replace(' ', '+'));
+//        			response.addCookie(username);
+        			response.sendRedirect(landingPage);
+        		} else { // password is wrong
+            		error = "Password is incorrect! Try again.";
+            		request.setAttribute("error", error);
+            		request.getRequestDispatcher("auth.jsp").include(request, response);
         		}
-        	} else {
-        		// User not found
-        		sendError("An account with this email does not exist.", request, response);
-        	} 
-        } catch (SQLException ex) {
-        	System.out.println(ex.getMessage());
+        	} else { // email is not in the database
+        		error = "Email does not exist! Please register first.";
+        		request.setAttribute("error", error);
+        		request.getRequestDispatcher("auth.jsp").include(request, response);
+        	}
+        	
+        } catch (SQLException e) {
+        	e.printStackTrace();
         }
-    }
+	}
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-     * response)
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        doGet(request, response);
-    }
-    
-    private void sendError(String error, HttpServletRequest request, HttpServletResponse response) {
-    	try {
-    		request.setAttribute("error", error);
-    		request.getRequestDispatcher("auth.jsp").include(request, response);
-    	} catch (Exception ex) {
-    		ex.printStackTrace();
-    	}
-    }
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
 }

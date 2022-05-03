21334f8387f802ce899ae582a56fb8f37e99780d
Sonia Kaul <soniakau@usc.edu>
2022-05-02 17:56:26 -0700

    Update CONSTANT.java
    goes with updates in login and register dispatchers

--- src/main/java/Util/Constant.java
+++ src/main/java/Util/Constant.java
@@ -3,7 +3,15 @@ package Util;
 import java.util.regex.Pattern;
 
 public class Constant {
+    //TODO replace it with your DB credentials and appropriate URL
     static public String DBUserName = "root";
-    static public String DBPassword = "Hesoyam1586";
-    static public String DBAddress = "jdbc:mysql://localhost:3306/TicTacToe?serverTimezone=PST";
+    static public String DBPassword = "root";
+    public static String url = "jdbc:mysql://localhost:3306/tictactoe";
+
+    static public Pattern namePattern = Pattern.compile("^[ A-Za-z]+$");
+    static public Pattern emailPattern = Pattern.compile("^[a-zA-Z0-9_+&*-]+(?:\\."
+            + "[a-zA-Z0-9_+&*-]+)*@"
+            + "(?:[a-zA-Z0-9-]+\\.)+[a-z"
+            + "A-Z]{2,7}$");
+
 }

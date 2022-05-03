3bf7f7542a0db32b8215dc422a2d8fdbe07a0a41
Sonia Kaul <soniakau@usc.edu>
2022-05-02 18:55:44 -0700

    Update Helper.java

--- src/main/java/Util/Helper.java
+++ src/main/java/Util/Helper.java
@@ -1,11 +1,12 @@
 package Util;
 
-import javax.servlet.ServletException;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.sql.SQLException;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.sql.SQLException;
 
 public class Helper {
 
@@ -17,19 +18,6 @@ public class Helper {
      */
     public static boolean validName(String name) {
         return Constant.namePattern.matcher(name).matches();
-    }
-
-    /**
-     * check if email is valid
-     *
-     * @param email the email user provides
-     * @return valid or not valid
-     */
-    public static boolean isValidEmail(String email) {
-        if (email == null) {
-            return false;
-        }
-        return Constant.emailPattern.matcher(email).matches();
     }
     
 	/**
@@ -63,57 +51,4 @@ public class Helper {
         }
         return ""; 
     }
-    
-
-    /**
-     * Get username with the email
-     *
-     * @param email
-     * @return userName
-     * @throws SQLException
-     */
-    public static String getUserName(String email) throws SQLException {
-        //TODO
-        return null;
-    }
-
-    /**
-     * Get userID with email
-     *
-     * @param email
-     * @return userID
-     * @throws SQLException
-     */
-    public static int getUserID(String email) throws SQLException {
-        //TODO
-        return 0;
-    }
-
-    /**
-     * check if the email and password matches
-     *
-     * @param email
-     * @param password
-     */
-    public static boolean checkPassword(String email, String password) throws SQLException {
-        //TODO
-        return false;
-    }
-
-    /** NOT USED, caught in exception
-     * Check if email is already registered
-     *
-     * @param email
-     * @param request
-     * @param response
-     * @return email registered or not
-     * @throws ServletException
-     * @throws IOException
-     */
-    public static boolean emailAlreadyRegistered(String email, HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        //TODO
-        return false;
-    }
 }
-

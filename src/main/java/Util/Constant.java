a2f1455da6b265932271abc2a0e17f4e0ab2f446
Sonia Kaul <soniakau@usc.edu>
2022-05-03 18:01:10 -0700

    tictac toe to TICTACTOE

--- src/main/java/Util/Constant.java
+++ src/main/java/Util/Constant.java
@@ -6,7 +6,7 @@ public class Constant {
     //TODO replace it with your DB credentials and appropriate URL
     static public String DBUserName = "root";
     static public String DBPassword = "root";
-    public static String url = "jdbc:mysql://localhost:3306/tictactoe";
+    public static String url = "jdbc:mysql://localhost:3306/TICTACTOE";
 
     static public Pattern namePattern = Pattern.compile("^[ A-Za-z]+$");
 }

b950011e2d66bb3a1af36f053956b4ab6a0aa4bb
Sonia Kaul <soniakau@usc.edu>
2022-05-05 18:17:36 -0700

    Update Constant.java

--- src/main/java/Util/Constant.java
+++ src/main/java/Util/Constant.java
@@ -3,10 +3,9 @@ package Util;
 import java.util.regex.Pattern;
 
 public class Constant {
-    //TODO replace it with your DB credentials and appropriate URL
     static public String DBUserName = "root";
-    static public String DBPassword = "root";
-    public static String url = "jdbc:mysql://localhost:3306/TICTACTOE";
-
+    static public String DBPassword = "Hesoyam1586";
+    static public String DBAddress = "jdbc:mysql://localhost:3306/TICTACTOE?serverTimezone=PST";
+    
     static public Pattern namePattern = Pattern.compile("^[ A-Za-z]+$");
 }
